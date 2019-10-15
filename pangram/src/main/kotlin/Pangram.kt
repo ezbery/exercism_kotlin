92eed2ebe464af1a4ee296f1ba730f066e640fb7
Bartosz Breninek <bartosz.breninek@rsqtechnologies.com>
2019-10-15 15:53:26 +0200

    solution + unignored tests

--- pangram/src/main/kotlin/Pangram.kt
+++ pangram/src/main/kotlin/Pangram.kt
@@ -1,5 +1,11 @@
 class Pangram {
+    private val allChars = "abcdefghijklmnopqrstuvwxyz"
     fun isPangram(input: String): Boolean {
-        return false
+        var result = true
+        allChars.forEach { c: Char ->
+            if (!(input.contains(c, ignoreCase = true)))
+                result = false
+        }
+        return result
     }
 }
