66c75189815db445b7097944fcf3d4f4a5fb8258
ezbery <brenin0204@gmail.com>
2019-10-29 17:45:27 +0100

    raindrops more flexible solution without code duplication

--- raindrops/src/main/kotlin/Raindrops.kt
+++ raindrops/src/main/kotlin/Raindrops.kt
@@ -1,17 +1,36 @@
 class Raindrops {
 
+    private val parsableFactors = listOf(3, 5, 7)
+
+    private fun findFactors(number: Int): List<Int> {
+        val numberFactors = mutableListOf<Int>()
+
+        for (i in 1..number) {
+            if (number % i == 0)
+                numberFactors.add(i)
+        }
+        return numberFactors
+    }
+
+    private fun changeToRaindropString(numbers: List<Int>): String {
+        var rainString = ""
+        if (numbers.isEmpty())
+            return ""
+        if (numbers.contains(parsableFactors[0]))
+            rainString += "Pling"
+        if (numbers.contains(parsableFactors[1]))
+            rainString += "Plang"
+        if (numbers.contains(parsableFactors[2]))
+            rainString += "Plong"
+        return rainString
+    }
+
     fun convert(input: Int): String {
-        var raindropString = ""
-        if (input % 3 == 0)
-            raindropString += "Pling"
-        if (input % 5 == 0)
-            raindropString += "Plang"
-        if (input % 7 == 0)
-            raindropString += "Plong"
-
-        if (raindropString.isEmpty())
-            raindropString = input.toString()
-
-        return raindropString
+        val inputFactors = findFactors(input)
+        val filteredInputFactors = inputFactors.filter { parsableFactors.contains(it) }
+        var rainString = changeToRaindropString(filteredInputFactors)
+        if (rainString.isEmpty())
+            rainString = input.toString()
+        return rainString
     }
 }
