9301402ab16cfc9f22dc032412e9e1ca645db6c0
ezbery <brenin0204@gmail.com>
2019-11-10 20:34:17 +0100

    static alphabet and function which generates diamond as numbers

--- diamond/src/main/kotlin/DiamondPrinter.kt
+++ diamond/src/main/kotlin/DiamondPrinter.kt
@@ -1,1 +1,15 @@
-class DiamondPrinter
+class DiamondPrinter {
+    private val alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    private val delimiter = " "
+
+    fun printToList(input: Char): List<String> {
+        val charIndex = alphabet.indexOf(input)
+        var diamondRows = mutableListOf<Int>()
+        for (i in 0..charIndex)
+            diamondRows.add(i)
+        for (i in charIndex - 1 downTo 0)
+            diamondRows.add(i)
+
+        return emptyList()
+    }
+}
