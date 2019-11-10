f4e868bc73affc3ff75fce7d34ee357ff937289f
ezbery <brenin0204@gmail.com>
2019-11-10 20:39:04 +0100

    function which change numbers into proper diamond row

--- diamond/src/main/kotlin/DiamondPrinter.kt
+++ diamond/src/main/kotlin/DiamondPrinter.kt
@@ -9,6 +9,19 @@ class DiamondPrinter {
         return string
     }
 
+    private fun generateDiamondRow(currentCharIndex: Int, maxCharIndex: Int): String {
+        val leadingSpaces = spaces(maxCharIndex - currentCharIndex)
+        val size = maxCharIndex * 2 + 1
+
+        val middleChars = if (size - 2 * leadingSpaces.length != 1)
+            alphabet[currentCharIndex] + spaces(size - 2 * leadingSpaces.length - 2) + alphabet[currentCharIndex]
+        else {
+            alphabet[currentCharIndex].toString()
+        }
+
+        return leadingSpaces + middleChars + leadingSpaces
+    }
+
     fun printToList(input: Char): List<String> {
         val charIndex = alphabet.indexOf(input)
         var diamondRows = mutableListOf<Int>()
