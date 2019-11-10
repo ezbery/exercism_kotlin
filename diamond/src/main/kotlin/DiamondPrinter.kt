6c3c3221f15de3c52a199765345d018c6a09dcc4
ezbery <brenin0204@gmail.com>
2019-11-10 20:39:27 +0100

    final mapping + tests unignored

--- diamond/src/main/kotlin/DiamondPrinter.kt
+++ diamond/src/main/kotlin/DiamondPrinter.kt
@@ -24,12 +24,12 @@ class DiamondPrinter {
 
     fun printToList(input: Char): List<String> {
         val charIndex = alphabet.indexOf(input)
-        var diamondRows = mutableListOf<Int>()
+        val diamondRows = mutableListOf<Int>()
         for (i in 0..charIndex)
             diamondRows.add(i)
         for (i in charIndex - 1 downTo 0)
             diamondRows.add(i)
 
-        return emptyList()
+        return diamondRows.map { generateDiamondRow(it, charIndex) }
     }
 }
