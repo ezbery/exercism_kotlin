890448d27b71f7be0956070948d12b1b3430f265
Bartosz Breninek <bartosz.breninek@rsqtechnologies.com>
2019-11-13 08:24:06 +0100

    removed @Throws

--- hamming/src/main/kotlin/Hamming.kt
+++ hamming/src/main/kotlin/Hamming.kt
@@ -1,7 +1,6 @@
 import java.lang.IllegalArgumentException
 
 class Hamming {
-    @Throws(Exception::class)
     fun compute(first: String, second: String): Int {
         if (first.length != second.length)
             throw IllegalArgumentException("left and right strands must be of equal length.")
