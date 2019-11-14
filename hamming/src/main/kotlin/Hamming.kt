0a24dc01c0f875f7405c6bac5091a9e72abe46b5
Bartosz Breninek <bartosz.breninek@rsqtechnologies.com>
2019-11-14 08:31:39 +0100

    decomposed pair in lambda

--- hamming/src/main/kotlin/Hamming.kt
+++ hamming/src/main/kotlin/Hamming.kt
@@ -2,6 +2,6 @@ class Hamming {
     fun compute(first: String, second: String): Int {
         require(first.length == second.length) { "left and right strands must be of equal length." }
 
-        return first.zip(second).count { pair -> pair.first != pair.second }
+        return first.zip(second).count { (f, s) -> f != s }
     }
 }
