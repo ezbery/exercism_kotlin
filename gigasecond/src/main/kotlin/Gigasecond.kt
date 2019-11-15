71210aac8c13839a1d83daf98421a9f5fd9390e1
Bartosz Breninek <bartosz.breninek@rsqtechnologies.com>
2019-11-15 11:37:53 +0100

    changed mapping LocalDate->LocalDateTime to LocalDate.AtStartOfDay()

--- gigasecond/src/main/kotlin/Gigasecond.kt
+++ gigasecond/src/main/kotlin/Gigasecond.kt
@@ -5,13 +5,5 @@ class Gigasecond(dateTime: LocalDateTime) {
     private val gigasecondInSeconds: Long = 1_000_000_000
     val date: LocalDateTime = dateTime.plusSeconds(gigasecondInSeconds)
 
-    constructor(localDate: LocalDate) : this(
-        LocalDateTime.of(
-            localDate.year,
-            localDate.month,
-            localDate.dayOfMonth,
-            0,
-            0
-        )
-    )
+    constructor(localDate: LocalDate) : this(localDate.atStartOfDay())
 }
