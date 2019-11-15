266abccf232758ae5929ff06687f21457132c3ff
Bartosz Breninek <bartosz.breninek@rsqtechnologies.com>
2019-11-15 13:44:55 +0100

    proper translation of vowels and consonants (common)

--- pig-latin/src/main/kotlin/PigLatin.kt
+++ pig-latin/src/main/kotlin/PigLatin.kt
@@ -1,6 +1,13 @@
 class PigLatin {
+    private val vowels = listOf('a', 'e', 'i', 'o', 'u')
+    private val consonants = ('a'..'z').filterNot { it in vowels }
+    private val pigLatinEnding = "ay"
 
     fun translate(input: String): String {
-        return input
+        return when {
+            input.first() in vowels -> input
+            input.first() in consonants -> input.drop(1) + input.take(1)
+            else -> ""
+        } + pigLatinEnding
     }
 }
