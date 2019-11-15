6f26eb01a9f1db8a7bf5e39dd27dbaab0cb08371
Bartosz Breninek <bartosz.breninek@rsqtechnologies.com>
2019-11-15 13:46:59 +0100

    special consonants list + handling

--- pig-latin/src/main/kotlin/PigLatin.kt
+++ pig-latin/src/main/kotlin/PigLatin.kt
@@ -1,12 +1,18 @@
 class PigLatin {
     private val vowels = listOf('a', 'e', 'i', 'o', 'u')
     private val consonants = ('a'..'z').filterNot { it in vowels }
+    private val specialConsonants = listOf("ch", "qu", "th", "rh", "thr", "sch")
     private val pigLatinEnding = "ay"
 
     fun translate(input: String): String {
         return when {
             input.first() in vowels -> input
-            input.first() in consonants -> input.drop(1) + input.take(1)
+            input.first() in consonants ->
+                when {
+                    input.take(3) in specialConsonants -> input.drop(3) + input.take(3)
+                    input.take(2) in specialConsonants -> input.drop(2) + input.take(2)
+                    else -> input.drop(1) + input.take(1)
+                }
             else -> ""
         } + pigLatinEnding
     }
