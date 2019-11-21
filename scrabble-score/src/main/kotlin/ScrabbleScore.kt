c8ca7e94bf1937cc26f80d9781dc732b8124ff07
ezbery <brenin0204@gmail.com>
2019-11-21 21:11:39 +0100

    letters and their scores in map

--- scrabble-score/src/main/kotlin/ScrabbleScore.kt
+++ scrabble-score/src/main/kotlin/ScrabbleScore.kt
@@ -1,4 +1,12 @@
 object ScrabbleScore {
+    private val lettersScore = mapOf(
+            Pair(listOf('A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'), 1),
+            Pair(listOf('D', 'G'), 2),
+            Pair(listOf('B', 'C', 'M', 'P'), 3),
+            Pair(listOf('F', 'H', 'V', 'W', 'Y'), 4),
+            Pair(listOf('K'), 5),
+            Pair(listOf('J', 'X'), 8),
+            Pair(listOf('Q', 'Z'), 10))
 
     fun scoreLetter(c: Char): Int {
         TODO("Implement this function to complete the task")
